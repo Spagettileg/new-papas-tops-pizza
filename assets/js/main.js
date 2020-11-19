dfaf40e135ac369bbd69af9f2c8b842a90e01ad0
Ubuntu <ubuntu@ip-172-31-34-198.eu-west-1.compute.internal>
2020-11-19 11:28:20 +0000

    Carousel controllers added to activate the carousel, enable indicators and left right controls too.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
+// **** Navbar Toggle Control ****
+
 document.querySelector('.menu').addEventListener
 ('click', ()=>{
     document.querySelectorAll('.target').forEach((item) => {
         item.classList.toggle('change');
     });
-})
+});
+
+// **** Carousel Controllers ****
+
+// ** Activate Carousel (powered by Bootstrap) **
+$("#myCarousel").carousel();
+
+// ** Enable Carousel Indicators **
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// ** Enable Carousel Controls **
+$(".carousel-control-prev").click(function(){
+  $("#myCarousel").carousel("prev");
+});
